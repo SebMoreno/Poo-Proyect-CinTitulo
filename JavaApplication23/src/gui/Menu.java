12b25c7071968a3063d475060458e6b9bf719ac9
SebMoreno <juansebasmorenoruiz@gmail.com>
2019-06-23 16:33:23 -0500

    Create metodo main

--- JavaApplication23/src/gui/Menu.java
+++ JavaApplication23/src/gui/Menu.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gui;
 
-/**
- *
- * @author Usuario
- */
 public class Menu {
-    
+    public static void main(String[] args){
+        System.out.println("Vamos a trabajar con un menú de consola para pobres");
+    }
 }
