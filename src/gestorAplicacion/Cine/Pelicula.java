44f7aae5f74444f253b55b9fbb7e0d221e443cd8
SebMoreno <juansebasmorenoruiz@gmail.com>
2019-06-23 19:28:32 -0500

    Creacion Sala class

--- src/gestorAplicacion/Cine/Pelicula.java
+++ src/gestorAplicacion/Cine/Pelicula.java
@@ -8,62 +8,6 @@ public class Pelicula {
     private String titulo;
     private String duracion;
     private String idioma;
-    private int precio;
-
-    public String getHora() {
-        return hora;
-    }
-
-    public void setHora(String hora) {
-        this.hora = hora;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getClasificacion() {
-        return clasificacion;
-    }
-
-    public void setClasificacion(String clasificacion) {
-        this.clasificacion = clasificacion;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDuracion() {
-        return duracion;
-    }
-
-    public void setDuracion(String duracion) {
-        this.duracion = duracion;
-    }
-
-    public String getIdioma() {
-        return idioma;
-    }
-
-    public void setIdioma(String idioma) {
-        this.idioma = idioma;
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(int precio) {
-        this.precio = precio;
-    }
+    
 
 }
--- src/gestorAplicacion/Cine/Sala.java
+++ src/gestorAplicacion/Cine/Sala.java
@@ -2,4 +2,60 @@ package gestorAplicacion.Cine;
 
 public class Sala {
 
+    private final int precio;
+    private final Character tipo;
+    private final Short capacidad;
+    private final Silla[][] sillas;
+    private Pelicula pelicula;
+    private Short sillasOcupadas;
+
+    public Sala(int precio, Character tipo, Short capacidad, Silla[][] sillas) {
+        this.precio = precio;
+        this.sillas = sillas;
+        this.tipo = tipo;
+        this.capacidad = capacidad;
+    }
+
+    public Sala(int precio, Character tipo, Short capacidad, Silla[][] sillas, Pelicula pelicula) {
+        this(precio, tipo, capacidad, sillas);
+        this.pelicula = pelicula;
+    }
+
+    public Sala(int precio, Character tipo, Short capacidad, Silla[][] sillas, Short sillasOcupadas) {
+        this(precio, tipo, capacidad, sillas);
+        this.sillasOcupadas = sillasOcupadas;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public Character getTipo() {
+        return tipo;
+    }
+
+    public Short getCapacidad() {
+        return capacidad;
+    }
+
+    public Silla[][] getSillas() {
+        return sillas;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public Short getSillasOcupadas() {
+        return sillasOcupadas;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public void setSillasOcupadas(Short sillasOcupadas) {
+        this.sillasOcupadas = sillasOcupadas;
+    }
+
 }
