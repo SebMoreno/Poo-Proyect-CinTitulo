1e9c2808518c3eb526f7183ef6908e448911df80
Cristian Mejía <crmejiam@unal.edu.co>
2019-06-29 10:06:43 -0500

    @jaironmayers @marianabetancur18

--- src/uiMain/OpcionDeMenu.java
+++ src/uiMain/OpcionDeMenu.java
@@ -1,13 +1,24 @@
 package uiMain;
 
 public abstract class OpcionDeMenu extends MenuDeConsola{
-
+        private String descripcion;
 	public abstract void ejecutar ();//clase abstracta de todos los menus
 	
 	
         @Override
 	public abstract String toString ();//ya que esto me ayudara a imprimir lo que hare con el objeto
 	//Es necesario crear constructor con lo que hará el toString para imprimir por pantalla
+        
+        //Codigo toString y constructor
+        /* OpcionX(String description){
+            this.descripcion=description;
+        }
+            toString{
+                System.out.println(descripcion);
+        }
+        */
+
+            
 	
 	}
 	
