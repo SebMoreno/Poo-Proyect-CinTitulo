1b8593d86507d76c7a6cc875997b857a840dacb1
Cristian Mejía <crmejiam@unal.edu.co>
2019-06-29 11:40:12 -0500

    creacion de comprarboleta y registraruser @jaironmayers @marianabetancur18

---
+++ src/uiMain/RegistrarNuevoUsuario.java
@@ -0,0 +1,15 @@
+package uiMain;
+
+public class RegistrarNuevoUsuario extends OpcionDeMenu {
+    private String titulomenu;
+    
+    public void ejecutar(){
+    //Llamar constructor y terminar método (sebas malparido)
+    }
+    
+@Override    
+    public String toString(){
+        return titulomenu; 
+}    
+    
+}
