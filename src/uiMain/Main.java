29352960b7290cab90d7a965b333b05cd74f8779
jaironmayers <52019911+jaironmayers@users.noreply.github.com>
2019-07-23 16:12:11 -0500

    Update Main.java

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -3,16 +3,18 @@ package uiMain;
 import baseDeDatos.Registro;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import gestorAplicacion.cine.Sala;
+import gestorAplicacion.cine.Silla;
 public class Main {
 
     static public int navegador = 1;
 
     public static void main(String[] args) {
-
-        Registro.readAllTxt();
-        Registro.god_menu_birth();
+        
+        
         Scanner scan = new Scanner(System.in);
+        /*Registro.readAllTxt();
+        Registro.god_menu_birth();
         ArrayList<OpcionDeMenu> menu_1 = new ArrayList<>();
         menu_1.add(Registro.menu_generico.get("iniciar sesion"));
         menu_1.add(Registro.menu_generico.get("iniciar como invitado"));
@@ -23,7 +25,9 @@ public class Main {
         while (navegador != 0) {
             option = scan.nextInt();
             menu_1.get(option - 1).ejecutar();
-        }
+        }*/
+        System.out.println("silla");
+        
     }
 
 }
