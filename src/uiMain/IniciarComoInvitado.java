6b3b255536f93a82d29c9871bdad45faf31a639d
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-07-24 05:04:28 -0500

    Update IniciarComoInvitado.java

--- src/uiMain/IniciarComoInvitado.java
+++ src/uiMain/IniciarComoInvitado.java
@@ -1,13 +1,13 @@
 package uiMain;
 
 import gestorAplicacion.usuario.Invitado;
-
+//tipo de usuario no registrado 
 public class IniciarComoInvitado extends OpcionDeMenu {
 
     private String titulomenu = "Iniciar como invitado";
 
     @Override
-    public void ejecutar() {
+    public void ejecutar() {//metodo que lanza el menu
         Main.usuarioActivo = new Invitado();
         Main.menu = Invitado.getMenu();
     }
