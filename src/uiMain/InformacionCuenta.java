483313a93f8d9a41434e8ef17872de56fcb9bd4d
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-07-24 05:02:40 -0500

    comentarios de las opciones info

--- src/uiMain/InformacionCuenta.java
+++ src/uiMain/InformacionCuenta.java
@@ -2,13 +2,13 @@ package uiMain;
 
 import gestorAplicacion.usuario.Cliente;
 import gestorAplicacion.usuario.Cuenta;
-//opcion informacion cuenta dispoible para todos los usuarios 
+//opcion informacion cuenta dispoible para usuarios registrados
 public class InformacionCuenta extends OpcionDeMenu {
 
     private final String titulomenu = "Mostrar la informacion de mi cuenta";
 
     @Override
-    public void ejecutar() {//accede al hash he imprime todas las keys y values(recordar que los values de nuestros hash map son arreglos)
+    public void ejecutar() {//accede al hash he imprime todas las keys y values(recordar que los values de nuestros hash map son arreglos, tiene en cuenta el usuario con el que se incio sesion
         Cliente us = (Cliente)Main.usuarioActivo;
         System.out.println("************" + "Cuenta del usuario " + us.getUsuario() + "************");
         System.out.println("Saldo: " + us.getCuenta().getSaldo());
