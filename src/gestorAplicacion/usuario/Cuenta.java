556ed191a6a5d14ef7dbad6c8f264ae4d33eac22
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-07-24 04:38:53 -0500

    comentarios cuenta

--- src/gestorAplicacion/usuario/Cuenta.java
+++ src/gestorAplicacion/usuario/Cuenta.java
@@ -13,11 +13,12 @@ public class Cuenta {
     private final ArrayList<Integer> idBoletas = new ArrayList<>();
     public static final HashMap<String, String[]> cuentasList = new HashMap<>(); // key: usuario    value: saldo, idBoleta1, idBoleta2, ... , idBoletaN
 
-    public static void RT(){
+    public static void RT(){//metodo necesario para leer el archivo donde se guardan las cuentas
         readTxt("cuentas.txt", cuentasList);
     }
 
-    public Cuenta(Cliente propietario, boolean existe) {
+    public Cuenta(Cliente propietario, boolean existe) {//tener en cuenta que una cuenta solo se genera cuando se crea un usuario  entonces recordar que en ocacione
+                                                        //se crea un usuario solo para acceder a metodo entonces tambien hay que tener ese filtro para no guardar una cuenta dos veces
         this.propietario = propietario;
         if (existe) {
             this.saldo = Integer.valueOf(cuentasList.get(propietario.getUsuario())[0]);
@@ -48,7 +49,7 @@ public class Cuenta {
         return cuentasList;
     }
 
-    public void setSaldo(int saldo) {
+    public void setSaldo(int saldo) {//metodo utilizado para reemplazar saldo
         this.saldo = saldo;
         String[] aux = cuentasList.get(propietario.getUsuario());
         aux[0] = Integer.toString(saldo);
