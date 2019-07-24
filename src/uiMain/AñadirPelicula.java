a22fab5ebe3f59a0746285cbb5537dd73921d082
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-07-24 04:48:56 -0500

    Update AñadirPelicula.java

--- src/uiMain/AñadirPelicula.java
+++ src/uiMain/AñadirPelicula.java
@@ -2,18 +2,18 @@ package uiMain;
 
 import gestorAplicacion.cine.Pelicula;
 import java.util.Scanner;
-
+//opcion de menu para añadir peliculas (es unica de administrador)
 public class AñadirPelicula extends OpcionDeMenu{
     
     private final String titulomenu = "Añadir Película";
 
-    public void ejecutar() {
+    public void ejecutar() {//metodo abstracto heredado opcion de menú
         Scanner esc = new Scanner(System.in);
         System.out.println("Escribe los datos de la nueva película");
         System.out.print("Titulo: ");
         String titulo = esc.next();
         System.out.println();
-        while (Pelicula.getPelisList().containsKey(titulo)) {
+        while (Pelicula.getPelisList().containsKey(titulo)) {//verifica que no quiera añadir una pelicula que ya existe
             System.out.println("Una película con ese titulo ya existe, copia uno diferente");
             titulo = esc.next();
         }
@@ -25,7 +25,7 @@ public class AñadirPelicula extends OpcionDeMenu{
         String duracion = esc.next();
         System.out.print("Idioma: ");
         String idioma = esc.next();
-        new Pelicula(titulo, genero, clasif, duracion, idioma);
+        new Pelicula(titulo, genero, clasif, duracion, idioma);//cuando llama al constructor el se encarga de guardarlo en el txt
     }
 
     public String toString() {
