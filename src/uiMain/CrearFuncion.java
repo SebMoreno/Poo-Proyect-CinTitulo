94214c7014c71722872e1e0f897d118251ca846c
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-07-24 04:52:48 -0500

    Update CrearFuncion.java

--- src/uiMain/CrearFuncion.java
+++ src/uiMain/CrearFuncion.java
@@ -5,15 +5,15 @@ import gestorAplicacion.cine.Pelicula;
 import gestorAplicacion.cine.Sala;
 import java.util.Map;
 import java.util.Scanner;
-
+//opcion de menu para crearfunciones (es unica de administrador)
 public class CrearFuncion extends OpcionDeMenu{
     
     private String titulomenu = "Crear Funcion";
     Scanner input = new Scanner(System.in);
     @Override
-    public void ejecutar() {
-        Informacionpelicula.imprimirPeliculas();
-        for(Map.Entry<String, String[]> entry : gestorAplicacion.cine.Sala.getSalasList().entrySet()){
+    public void ejecutar() {//metodo abstracto ejecutar
+        Informacionpelicula.imprimirPeliculas();//mirar descripcion en la clase informacion pelicula
+        for(Map.Entry<String, String[]> entry : gestorAplicacion.cine.Sala.getSalasList().entrySet()){//escoja la sala para la funcion
             System.out.println("******** ID: " + entry.getKey() + " ********");
             System.out.println("Precio: " + entry.getValue()[0]);
             System.out.println("Tipo:   " + entry.getValue()[1]);
@@ -26,7 +26,7 @@ public class CrearFuncion extends OpcionDeMenu{
         String hora = input.next();
         System.out.print("Ingrese una película para reproducir en la función: ");
         String pelicula = input.next();
-        Pelicula peli = new Pelicula(pelicula);
+        Pelicula peli = new Pelicula(pelicula);//los constructores se encargan de crear eso
         
         Funcion funcion = new Funcion(sala,hora,peli);
     }
