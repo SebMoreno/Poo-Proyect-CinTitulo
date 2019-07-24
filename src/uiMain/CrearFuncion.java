7bef63c22e688f2c0108f3f2d2ff7828a9dc1364
Cristian Mejía <crmejiam@unal.edu.co>
2019-07-24 02:19:43 -0500

    Update CrearFuncion.java

--- src/uiMain/CrearFuncion.java
+++ src/uiMain/CrearFuncion.java
@@ -1,12 +1,33 @@
 package uiMain;
 
+import gestorAplicacion.cine.Funcion;
+import gestorAplicacion.cine.Pelicula;
+import gestorAplicacion.cine.Sala;
+import java.util.Map;
+import java.util.Scanner;
+
 public class CrearFuncion extends OpcionDeMenu{
     
     private String titulomenu = "Crear Funcion";
-    
+    Scanner input = new Scanner(System.in);
     @Override
     public void ejecutar() {
+        System.out.println("Ingrese el ID de una sala");
+        for(Map.Entry<String, String[]> entry : gestorAplicacion.cine.Sala.getSalasList().entrySet()){
+            System.out.println("******** ID: " + entry.getKey() + " ********");
+            System.out.println("Precio: " + entry.getValue()[0]);
+            System.out.println("Tipo:   " + entry.getValue()[1]);
+            System.out.println("Capacidad:  " + entry.getValue()[2]);
+        }
+        String ID = input.next();
+        Sala sala = new Sala(ID);
+        System.out.println("Ingrese una hora para la función");
+        String hora = input.next();
+        System.out.println("Ingrese una película para reproducir en la función");
+        String pelicula = input.next();
+        Pelicula peli = new Pelicula(pelicula);
         
+        Funcion funcion = new Funcion(sala,hora,peli);
     }
 
     @Override
