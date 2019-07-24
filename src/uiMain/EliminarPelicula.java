a2f2e84cc5f0e4577bbe06758ba71f8b46abfb96
Cristian Mejía <crmejiam@unal.edu.co>
2019-07-24 01:49:06 -0500

    Update EliminarPelicula.java

--- src/uiMain/EliminarPelicula.java
+++ src/uiMain/EliminarPelicula.java
@@ -1,12 +1,18 @@
 package uiMain;
 
+import java.util.Scanner;
+
 public class EliminarPelicula extends OpcionDeMenu {
     
     private String titulomenu = "Eliminar Pelicula";
-    
+    Scanner input = new Scanner (System.in);
     @Override
     public void ejecutar() {
-        
+        System.out.println("Ingrese el título de la película a eliminar");
+        Informacionpelicula.imprimirPeliculas();
+        String titulo = input.next();
+        gestorAplicacion.cine.Pelicula.getPelisList().remove(titulo);
+        System.out.println("¡Película eliminada con exito!");
     }
 
     @Override
