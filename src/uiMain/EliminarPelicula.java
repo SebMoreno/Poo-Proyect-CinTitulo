c28d16e8ea003048e6a0d80b69f59dd3617bde4b
marianabetancur18 <51976569+marianabetancur18@users.noreply.github.com>
2019-07-24 04:56:26 -0500

    Update EliminarPelicula.java

--- src/uiMain/EliminarPelicula.java
+++ src/uiMain/EliminarPelicula.java
@@ -3,13 +3,13 @@ package uiMain;
 import baseDeDatos.Data;
 import gestorAplicacion.cine.Pelicula;
 import java.util.Scanner;
-
+//opcion de menu para borrar peliculas (es unica de administrador)
 public class EliminarPelicula extends OpcionDeMenu {
     
     private String titulomenu = "Eliminar Pelicula";
     Scanner input = new Scanner (System.in);
     @Override
-    public void ejecutar() {
+    public void ejecutar() {//elimina la pelicula del hash he invoca al write para escribir nuevamente en el txt
         Informacionpelicula.imprimirPeliculas();
         System.out.println("Ingrese el título exacto de la película a eliminar");        
         String titulo = input.next();
